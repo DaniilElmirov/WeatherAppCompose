31e1cbdaecbbb55de7083124c811a0fdc0e6d148
Ельмиров Даниил Владимирович <elmirovwork@gmail.com>
2024-01-07 14:48:05 +0700

    Добавил сущности в domain слой

---
+++ app/src/main/java/com/elmirov/weatherappcompose/domain/entity/City.kt
@@ -0,0 +1,7 @@
+package com.elmirov.weatherappcompose.domain.entity
+
+data class City(
+    val id: Int,
+    val name: String,
+    val country: String,
+)
---
+++ app/src/main/java/com/elmirov/weatherappcompose/domain/entity/Weather.kt
@@ -0,0 +1,11 @@
+package com.elmirov.weatherappcompose.domain.entity
+
+import java.util.Calendar
+
+data class Weather(
+    val tempCelsius: Float,
+    val conditionText: String,
+    val conditionUrl: String,
+    val date: Calendar,
+
+)
